d845b9659b4c3adfa6260487c6b4fbd6606622bb
Sankara Subramaniyan <sankarasubramaniyan.ab@gmail.com>
2021-12-17 19:38:38 +0530

    Added flask API

--- js/scripts.js
+++ js/scripts.js
@@ -33,42 +33,54 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 function validate() {
-    var student_ans=document.getElementById("studenttextarea").value;
-    var given_ans=document.getElementById("keytextarea").value;
-    var mark=document.getElementById("scoretext").value;
-    var method=document.getElementById("method").value;
-    if(student_ans==""||given_ans==""||mark=="")
-    {
+    var student_ans = document.getElementById("studenttextarea").value;
+    var given_ans = document.getElementById("keytextarea").value;
+    var mark = document.getElementById("scoretext").value;
+    var method = document.getElementById("method").value;
+    if (student_ans == "" || given_ans == "" || mark == "") {
         return false;
     }
-    var data={
-        "student_ans":student_ans,
-        "given_ans":given_ans,
-        "mark":mark,
-        "method":method
+    var data = {
+        "student_ans": student_ans,
+        "given_ans": given_ans,
+        "mark": mark,
+        "method": method
     };
     let options = {
         method: 'POST',
         headers: {
-            'Content-Type': 
-                'application/json;charset=utf-8'
+            'Content-Type': 'application/json;charset=utf-8',
+            'mode': 'no-cors'
         },
         body: JSON.stringify(data)
     }
+
+    if(method=="word2vec"){
     let fetchRes = fetch(
-        "http://dummy.restapiexample.com/api/v1/create", 
-                                                options);
-                fetchRes.then(res =>
-                    res.json()).then(d => {
-                        const obj = JSON.parse(d);
-                        
-                        console.log(d)
-                    })
-
-
-    var parent= document.getElementById("score");
-    parent.innerHTML="";
-    var child = document.createElement("div");
-    child.innerHTML = "<div class='TxtTile'>Similarity</div><div class='pInfo'>25%</div>";
-    parent.appendChild(child);
-  }
+        "http://127.0.0.1:4458/grades/word2vec",
+        options);
+    fetchRes.then(res => res.json()).
+        then(d => {
+            console.log(d);
+            parent = document.getElementById("score");
+            parent.innerHTML = "";
+            var child = document.createElement("div");
+            child.innerHTML = "<div class='TxtTile'>Similarity</div><div class='pInfo'>"+d.grade+"</div>";
+            parent.appendChild(child);
+        });
+    }
+    else{
+        let fetchRes = fetch(
+            "http://127.0.0.1:4458/grades/doc2vec",
+            options);
+        fetchRes.then(res => res.json()).
+            then(d => {
+                console.log(d);
+                parent = document.getElementById("score");
+                parent.innerHTML = "";
+                var child = document.createElement("div");
+                child.innerHTML = "<div class='TxtTile'>Similarity</div><div class='pInfo'>"+d.grade+"</div>";
+                parent.appendChild(child);
+            });
+    }
+}
